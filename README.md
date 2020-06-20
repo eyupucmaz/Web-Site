49f96cf8fd803793b32f9152b59ac7cb8d504f80
eyupucmaz <eyup@DESKTOP-9MGQVMD.localdomain>
2020-06-21 02:57:17 +0300

    File added

---
+++ README.md
@@ -0,0 +1,1 @@
+# eyupucmaz.github.io
---
+++ src/app.js
@@ -0,0 +1,10 @@
+let checkbox = document.querySelector(".switch-input");
+
+checkbox.addEventListener("change", function () {
+     let theme = document.querySelector("#theme");
+     if (this.checked) {
+          theme.setAttribute("href", "./style/dark.css");
+     } else {
+          theme.setAttribute("href", "./style/main.css");
+     }
+});
