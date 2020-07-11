a9eaf026766a9d4574caf457009fdf0a160c5c74
Eyup UCMAZ <eyupucmaz@gmail.com>
2020-07-12 02:26:29 +0300

    test

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,11 +1,11 @@
 let checkbox = document.querySelector(".switch-input");
 
 checkbox.addEventListener("change", function () {
-     let theme = document.querySelector("#theme");
-     if (this.checked) {
-          theme.setAttribute("href", "./style/dark.css");
-     } else {
-          theme.setAttribute("href", "./style/main.css");
-     }
+	let theme = document.querySelector("#theme");
+	if (this.checked) {
+		theme.setAttribute("href", "./style/dark.css");
+	} else {
+		theme.setAttribute("href", "./style/main.css");
+	}
 });
-console.log("--------------\n-HELLO WORLD!-\n--------------");
+console.log("--------------\n-HELLO WORLD!!-\n--------------");
